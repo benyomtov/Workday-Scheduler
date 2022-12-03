2c30166ac33dc1d8aded2114f0259b1f5bbeddd4
Ben Yomtov <benyomtov@comcast.net>
2022-12-03 13:52:51 -0500

    added timed-contingent formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(function runScheduler() {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,13 +9,12 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   var saveBtn = $(".saveBtn");
+  var description = $(".description");
 
   saveBtn.on("click", function() {
-    var parentId = $(this).parent().attr("id");
-    alert(parentId);
-    
+    var parentEl = $(this).parent().attr("id");
+    alert(parentEl);
   });
-
 
   //
   // TODO: Add code to apply the past, present, or future class to each time
@@ -23,10 +22,33 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  
+var today = dayjs();
+var hours = [dayjs().hour(09), dayjs().hour(10), dayjs().hour(11), dayjs().hour(12), dayjs().hour(13), dayjs().hour(14), dayjs().hour(15), dayjs().hour(16), dayjs().hour(17)];
+var currentHour = today.format("HH");
+
+for (var i = 0; i < hours.length; i++) {
+  var timeBlock = $("body").children().eq(1).children().eq(i);
+  var hourFormatted = hours[i].format("HH");
+
+  console.log("timeBlock" + timeBlock);
+  console.log("hourFormatted" + hourFormatted);
+  console.log("currentHour" + currentHour);
+
+  if (hourFormatted < currentHour) {
+    timeBlock.addClass("past");
+  } else if(hourFormatted === currentHour) {
+    timeBlock.addClass("present");
+  } else {
+    timeBlock.addClass("future");
+  }
+}
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  
+
+
   // TODO: Add code to display the current date in the header of the page.
 });
